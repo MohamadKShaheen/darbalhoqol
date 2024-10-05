9ad6f6c7357fc02cf56334682bddf7128887b1cb
Mohamad K.Shaheen <Mohamadk.shaheen@gmail.com>
2024-10-05 22:53:15 +0300

    fixed map component styles

--- app/components/map.jsx
+++ app/components/map.jsx
@@ -1,9 +1,39 @@
 import styles from './map.module.css';
 
 const Map = () => {
+  const customers = [
+    {
+      className: "canada"
+    },
+    {
+      className: "oman",
+    },
+    {
+      className: "UAE"
+    },
+    {
+      className: "saudi"
+    },
+    {
+      className: "syria"
+    },
+    {
+      className: "egypt"
+    }
+  ]
   return (
     <div className={styles.container}>
-      <img className={styles.image} src="https://globalmaps.org/wp-content/uploads/2023/10/World-Map-white-2048x1171.png" alt=""/>
+      <div className={styles.mapHeader}>
+        <p>
+          Our Customers
+        </p>
+      </div>
+      <div className={styles.secondContainer}>
+        {customers.map((customer, index) => (
+          <img src="/images/pin.svg" alt="" className={styles[customer.className]} key={index}/>
+        ))}
+        <img className={styles.image} src="/images/World_map.jpg" alt=""/>
+      </div>
     </div>
   );
 }
