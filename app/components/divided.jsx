731332a331a85f7b37c6dd77b23d61c51e11d7d9
Mohamad K.Shaheen <Mohamadk.shaheen@gmail.com>
2024-10-11 19:08:01 +0300

    fixed divided responsive styles

--- app/components/divided.jsx
+++ app/components/divided.jsx
@@ -5,18 +5,18 @@ const Divided = () => {
     <div className={styles.container}>
       <div className={styles.first_half}>
         <div className={styles.centered_image}>
-          <img src="/images/factory.jpeg"/>
+          <img className={styles.second_half_image} src="/images/factory.jpeg"/>
         </div>
       </div>
       <div className={styles.second_half}>
         <div className={styles.full_image}>
-          <img src="/images/field.jpg"/>
+          <img className={styles.first_half_image} src="/images/field.jpg"/>
         </div>
         <div className={styles.text}>
           <h2 className={styles.header}>
             Why DARB ALHOOQUL?
           </h2>
-          <p>
+          <p className={styles.paragraph}>
             At DARB ALHOOQUL, we blend tradition with innovation to deliver the finest quality products. From carefully selected olives and fresh vegetables to artisanal pickles, sauces, and jams, every product is crafted with a commitment to purity and excellence. Our farm-to-table approach ensures that each item is sustainably sourced and packed with natural flavors, bringing you the best that nature has to offer. Trust DARB ALHOOQUL for premium products that are as fresh as they are authentic.
           </p>
         </div>
